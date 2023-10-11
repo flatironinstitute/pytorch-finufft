3ed25022b2eabd62432b34c736d4af273c424ff7
Brian Ward <bward@flatironinstitute.org>
2023-10-11 15:39:25 -0400

    Formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
-import torch
 import pytest
+import torch
+
 
 # custom: skip tests with 'cuda' in name when unavailable
 def pytest_collection_modifyitems(session, config, items):
     if not torch.cuda.is_available():
         for test in items:
-            if 'cuda' in test.name:
+            if "cuda" in test.name:
                 test.add_marker(pytest.mark.skip("CUDA unavailable"))
 
 
@@ -18,17 +19,19 @@ def pytest_runtest_makereport(item, call):
     outcome = yield
     rep = outcome.get_result()
 
-    if item.config.getoption('--error-for-skips'):
+    if item.config.getoption("--error-for-skips"):
         if rep.skipped and call.excinfo.errisinstance(pytest.skip.Exception):
-            rep.outcome = 'failed'
+            rep.outcome = "failed"
             r = call.excinfo._getreprcrash()
-            rep.longrepr = 'Forbidden skipped test - {message}'.format(message=r.message)
+            rep.longrepr = "Forbidden skipped test - {message}".format(
+                message=r.message
+            )
 
 
 def pytest_addoption(parser):
     parser.addoption(
-        '--error-for-skips',
-        action='store_true',
+        "--error-for-skips",
+        action="store_true",
         default=False,
-        help='Treat skipped tests as errors'
+        help="Treat skipped tests as errors",
     )
